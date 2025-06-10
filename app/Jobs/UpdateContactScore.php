b15873f0879cbe2e73eb2d017a0e3fd8ff65dee7
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2025-06-10 10:35:30 -0300

    SCRUM-6 ✨ : feat Configurar a Fila com Redis & Criar o Job de Pontuação

---
+++ app/Jobs/UpdateContactScore.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Contact;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Testing\Fluent\Concerns\Interaction;
+
+class UpdateContactScore implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * O contato que será processado por este job.
+     * O 'public' é importante para que o Laravel possa acessá-lo.
+     */
+    public Contact $contact;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(Contact $contact)
+    {
+        $this->contact = $contact;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        sleep(5);
+
+        $newScore = rand(0, 100);
+
+        $this->contact->update([
+            'score' => $newScore,
+            'updated_at' => now()
+        ]);
+    }
+}
